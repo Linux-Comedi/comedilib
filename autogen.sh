3f05c6147feffb7809354a0e34928ea5c2614ad2
David Schleef <ds@schleef.org>
2003-06-04 03:02:25 +0000

    touch some missing files

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+if ! -f NEWS ; then
+	touch NEWS
+fi
+if ! -f AUTHORS ; then
+	touch AUTHORS
+fi
+if ! -f ChangeLog ; then
+	touch ChangeLog
+fi
 
 aclocal -I m4 && \
 libtoolize --copy --force && \
