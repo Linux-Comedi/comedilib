3a85b2d8985f402270ef0b9d1b926ad23ef653b9
Frank Mori Hess <fmhess@speakeasy.net>
2005-02-06 16:17:51 +0000

    patch from beat.zahnd@phim.unibe.ch (Beat ZAHND), he says:
    I extendended the SCXI support for the 1122 module and moved the
    scxi_boards structure to scxi.c

--- include/comedilib_scxi.h
+++ include/comedilib_scxi.h
@@ -31,17 +31,6 @@ struct scxi_board_struct {
 
 typedef struct scxi_board_struct scxi_board_t;
 
-const scxi_board_t scxi_boards[] = {
-	{ 0, "unknown\0", 2, SLOW_INTERVAL, 0, 0, 0, 0, 0, 0, 0, 0 },
-	{ 0xffffffff, "empty\0", 2, SLOW_INTERVAL, 0, 0, 0, 0, 0, 0, 0, 0 },
-	{ 0x06, "SCXI-1100\0", 1, FAST_INTERVAL, SCXI_DIO_NONE, SCXI_AIO_AI, 32,
-		0x00, 0x00, 0x00, 0x00, 0x00 },
-	{ 0x1e, "SCXI-1102\0", 2, FAST_INTERVAL, SCXI_DIO_NONE, SCXI_AIO_AI, 32,
-		0x02, 0x05, 0x01, 0x03, 0x04 },
-	//{ 0x14, "SCXI-1124\0", 2, SLOW_INTERVAL, SCXI_DIO_NONE, SCXI_AIO_AO, 6,
-	//	0x02, 0x08, 0x00, 0x03, 0x00 },
-};
-
 #define n_scxi_boards ((sizeof(scxi_boards)/sizeof(scxi_boards[0])))
 
 struct scxi_module_struct {
--- scxi/scxi.c
+++ scxi/scxi.c
@@ -3,6 +3,19 @@
 #include <stdlib.h>
 #include <byteswap.h>
 
+const scxi_board_t scxi_boards[] = {
+       { 0, "unknown\0", 2, SLOW_INTERVAL, 0, 0, 0, 0, 0, 0, 0, 0 },
+       { 0xffffffff, "empty\0", 2, SLOW_INTERVAL, 0, 0, 0, 0, 0, 0, 0, 0 },
+       { 0x06, "SCXI-1100\0", 1, FAST_INTERVAL, SCXI_DIO_NONE, SCXI_AIO_AI, 32,
+               0x00, 0x00, 0x00, 0x00, 0x00 },
+       { 0x1e, "SCXI-1102\0", 2, FAST_INTERVAL, SCXI_DIO_NONE, SCXI_AIO_AI, 32,
+               0x02, 0x05, 0x01, 0x03, 0x04 },
+       { 0x0a, "SCXI-1122\0", 2, FAST_INTERVAL, SCXI_DIO_NONE, SCXI_AIO_AI, 16,
+               0x02, 0x01, 0x00, 0x03, 0x00 },
+       //{ 0x14, "SCXI-1124\0", 2, SLOW_INTERVAL, SCXI_DIO_NONE, SCXI_AIO_AO, 6,
+       //      0x02, 0x08, 0x00, 0x03, 0x00 },
+};
+
 static int scxi_identify(scxi_mod_t *mod);
 
 static int scxi_serial_config(comedi_t *it, unsigned int clock_interval)
