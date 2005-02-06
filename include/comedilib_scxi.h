cd07b7217bf438e4755387f9803a456fd48f32e8
Frank Mori Hess <fmhess@speakeasy.net>
2005-02-06 16:22:41 +0000

    fix license headers, and prevent multiple includes of comedilib_scxi.h

--- include/comedilib_scxi.h
+++ include/comedilib_scxi.h
@@ -1,3 +1,29 @@
+/*
+    include/comedilib_scxi.h
+    header file for the comedi library routines
+
+    COMEDI - Linux Control and Measurement Device Interface
+    Copyright (C) 1998-2002 David A. Schleef <ds@schleef.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#ifndef _COMEDILIB_SCXI_H
+#define _COMEDILIB_SCXI_H
+
 #include <comedilib.h>
 
 #define SLOT0_INTERVAL  1200
@@ -54,3 +80,5 @@ int comedi_scxi_register_readwrite(scxi_mod_t *mod, unsigned short reg_address, 
 #ifdef __cplusplus
 }
 #endif
+
+#endif	// _COMEDILIB_SCXI_H
